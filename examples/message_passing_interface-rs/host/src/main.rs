236d79dabc61bcf1925823c2928126031ff45f45
Wang Tao <2412433597@qq.com>
2025-11-13 21:38:00 +0800

    examples: enable message-passing-interface-rs example for no-std and promote it to a common example (#254)

--- examples/message_passing_interface-rs/host/src/main.rs
+++ examples/message_passing_interface-rs/host/src/main.rs
@@ -15,9 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use optee_teec::{
-    Context, ErrorKind, Operation, ParamNone, ParamTmpRef, ParamType, ParamValue, Uuid,
-};
+use optee_teec::{Context, ErrorKind, Operation, ParamNone, ParamTmpRef, Uuid};
 
 type Result<T> = optee_teec::Result<T>;
 
@@ -53,24 +51,23 @@ impl EnclaveClient {
 
     pub fn invoke(&mut self, input: &proto::EnclaveInput) -> Result<proto::EnclaveOutput> {
         let command_id = input.command as u32;
-        let mut serialized_input = proto::serde_json::to_vec(input).map_err(|e| {
+        let mut serialized_input = serde_json::to_vec(input).map_err(|e| {
             eprintln!("Failed to serialize input: {}", e);
             ErrorKind::BadParameters
         })?;
 
         let p0 = ParamTmpRef::new_input(serialized_input.as_mut_slice());
         let p1 = ParamTmpRef::new_output(&mut self.buffer);
-        let p2 = ParamValue::new(0, 0, ParamType::ValueInout);
 
-        let mut operation = Operation::new(0, p0, p1, p2, ParamNone);
+        let mut operation = Operation::new(0, p0, p1, ParamNone, ParamNone);
 
         let uuid = Uuid::parse_str(&self.uuid)?;
         let mut session = self.context.open_session(uuid)?;
         session.invoke_command(command_id, &mut operation)?;
-        let len = operation.parameters().2.a() as usize;
+        let len = operation.parameters().1.updated_size();
 
-        let output: proto::EnclaveOutput = proto::serde_json::from_slice(&self.buffer[0..len])
-            .map_err(|e| {
+        let output: proto::EnclaveOutput =
+            serde_json::from_slice(&self.buffer[0..len]).map_err(|e| {
                 eprintln!("Failed to deserialize output: {}", e);
                 ErrorKind::BadParameters
             })?;
